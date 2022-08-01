2fb19ce0297e4f798ab3e6ee2052622789b531ff
Rehan Yousaf <rehanyousaf147@gmail.com>
2022-08-01 07:10:50 +0500

    NAS-116410: Added roles card

--- src/app/pages/datasets/datasets.module.ts
+++ src/app/pages/datasets/datasets.module.ts
@@ -31,6 +31,7 @@ import { DatasetQuotaEditFormComponent } from 'app/pages/datasets/components/dat
 import { DatasetQuotasGrouplistComponent } from 'app/pages/datasets/components/dataset-quotas/dataset-quotas-grouplist/dataset-quotas-grouplist.component';
 import { DatasetQuotasUserlistComponent } from 'app/pages/datasets/components/dataset-quotas/dataset-quotas-userlist/dataset-quotas-userlist.component';
 import { DeleteDatasetDialogComponent } from 'app/pages/datasets/components/delete-dataset-dialog/delete-dataset-dialog.component';
+import { RolesCardComponent } from 'app/pages/datasets/components/roles-card/roles-card.component.component';
 import { ZvolFormComponent } from 'app/pages/datasets/components/zvol-form/zvol-form.component';
 import { routing } from 'app/pages/datasets/datasets.routing';
 import { EncryptionModule } from 'app/pages/datasets/modules/encryption/encryption.module';
@@ -83,6 +84,7 @@ import { SpaceManagementChartComponent } from './components/space-management-cha
     DatasetDetailsPanelComponent,
     DatasetCapacityManagementCardComponent,
     DatasetNodeComponent,
+    RolesCardComponent,
     DatasetQuotaAddFormComponent,
     DatasetQuotaEditFormComponent,
     DatasetQuotasUserlistComponent,
